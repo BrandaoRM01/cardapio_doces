a29ebe6bc7715e841768b1d506e4ff4fbfe35c9c
BrandaoRM01 <raulmb231@gmail.com>
2026-02-22 18:10:20 -0300

    Separando por categoria e embelezando os cards

--- assets/js/components/card.js
+++ assets/js/components/card.js
@@ -1,16 +1,19 @@
 import { formatarPreco } from "../utils/format.js";
 
 export function criarCard(doce) {
-    return `
+  return `
     <div class="col-md-4 mb-4">
-      <div class="card h-100 shadow-sm">
+      <div class="card doce-card h-100 shadow-sm">
         <img src="${doce.imagem}" class="card-img-top" alt="${doce.nome}">
-        <div class="card-body text-center">
-          <h5>${doce.nome}</h5>
-          <p>Peso: ${doce.peso}</p>
-          <span class="badge bg-success fs-6">
-            ${formatarPreco(doce.preco)}
-          </span>
+        <div class="card-body">
+          <h5 class="card-title text-center">${doce.nome}</h5>
+          <p class="card-text descricao">${doce.descricao}</p>
+          <p class="peso text-center">Peso: ${doce.peso}</p>
+          <div class="text-center">
+            <span class="preco">
+              ${formatarPreco(doce.preco)}
+            </span>
+          </div>
         </div>
       </div>
     </div>
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,31 @@ import { criarCard } from "./components/card.js";
 
 const container = document.getElementById("lista-doces");
 
+function agruparPorCategoria(lista) {
+    return lista.reduce((acc, doce) => {
+        if (!acc[doce.categoria]) {
+            acc[doce.categoria] = [];
+        }
+        acc[doce.categoria].push(doce);
+        return acc;
+    }, {});
+}
+
 function renderizar() {
     container.innerHTML = "";
 
-    doces.forEach(doce => {
-        container.innerHTML += criarCard(doce);
+    const categorias = agruparPorCategoria(doces);
+
+    Object.keys(categorias).forEach(categoria => {
+        container.innerHTML += `
+      <div class="col-12 mt-4">
+        <h2 class="categoria-titulo">${categoria}</h2>
+      </div>
+    `;
+
+        categorias[categoria].forEach(doce => {
+            container.innerHTML += criarCard(doce);
+        });
     });
 }
 
