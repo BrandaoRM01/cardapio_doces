a6d84dc0366ff04697d59659ab0c9a79500e4916
BrandaoRM01 <raulmb231@gmail.com>
2026-02-22 18:02:29 -0300

    Adicionando doces no cardápio

--- assets/js/data/doces.js
+++ assets/js/data/doces.js
@@ -1,16 +1,72 @@
 export const doces = [
     {
-        nome: "Brigadeiro Gourmet",
-        preco: 4.5,
-        peso: "50g",
-        imagem: "assets/img/brigadeiro.jpg",
-        categoria: "brigadeiros"
+        nome: "Ovo de Ninho",
+        preco: 70,
+        peso: "350g",
+        imagem: "assets/img/ovo-ninho-colher.jpg",
+        categoria: "ovos de colher"
     },
     {
-        nome: "Bolo de Pote Ninho",
-        preco: 12,
-        peso: "250g",
-        imagem: "assets/img/bolo.jpg",
-        categoria: "bolos"
+        nome: "Ovo de Brigadeiro",
+        preco: 70,
+        peso: "350g",
+        imagem: "assets/img/ovo-brigadeiro-colher.jpg",
+        categoria: "ovos de colher"
+    },
+    {
+        nome: "Ovo de Óreo",
+        preco: 72,
+        peso: "350g",
+        imagem: "assets/img/ovo-oreo-colher.jpg",
+        categoria: "ovos de colher"
+    },
+    {
+        nome: "Ovo de Maracuja",
+        preco: 75,
+        peso: "350g",
+        imagem: "assets/img/ovo-maracuja-colher.jpg",
+        categoria: "ovos de colher"
+    },
+    {
+        nome: "Ovo de Ninho com Morango",
+        preco: 70,
+        peso: "350g",
+        imagem: "assets/img/ovo-ninho-morango-colher.jpg",
+        categoria: "ovos de colher"
+    },
+    {
+        nome: "Ovo de Ninho com Nutella",
+        preco: 75,
+        peso: "350g",
+        imagem: "assets/img/ovo-ninho-nutella-colher.jpg",
+        categoria: "ovos de colher"
+    },
+    {
+        nome: "Ovo de Kinder",
+        preco: 80,
+        peso: "350g",
+        imagem: "assets/img/ovo-kinder-colher.jpg",
+        categoria: "ovos de colher"
+    },
+    {
+        nome: "Ovo de Ferrero Rocher",
+        preco: 70,
+        peso: "350g",
+        imagem: "assets/img/ovo-ferrero-rocher-colher.jpg",
+        categoria: "ovos de colher"
+    },
+    {
+        nome: "Kit com 3 Unidades",
+        preco: 35,
+        peso: "50g",
+        imagem: "assets/img/ovo-3-unidades.jpg",
+        categoria: "kit mini ovos"
+    },
+    {
+        nome: "Kit com 6 Unidades",
+        preco: 65,
+        peso: "50g",
+        imagem: "assets/img/ovos-6-unidades.jpg",
+        categoria: "kit mini ovos"
     }
 ];
